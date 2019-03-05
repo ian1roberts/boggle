2926aca8e45789d8ebf8ecfe427f741ad2ff645c
Ian Roberts <ian.roberts@cantab.net>
2019-03-05 09:56:41 +0000

    refactored board test

--- test/test_board.py
+++ test/test_board.py
@@ -2,9 +2,11 @@
 
 
 import unittest
+import multiprocessing
 from boggle.grid import Grid
 from boggle.moves import Moves
-from boggle.paths import make_digraph, compute_all_paths
+from boggle.paths import do_chains_to_words, do_compute_chains
+from boggle.manage import load_dictionary
 
 
 class Test(unittest.TestCase):
@@ -12,19 +14,27 @@ class Test(unittest.TestCase):
 
     grid = Grid("cat dog hog")
     moves = Moves(grid)
-    p3 = Paths(grid, 3)
+    p = multiprocessing.Pool(4)
+    xargs = []
+    for coord in grid.coords:
+        xargs.append((coord, moves, 3))  # maxwlen=3
+
+    board = p.map(do_compute_chains, xargs)
+    dictionary = load_dictionary()
+    all_words = do_chains_to_words(grid, board, dictionary)
 
     def test_instantiate_board(self):
         """Test build board for single fixed wlen path."""
-        b = Board(3, Test.p3.grid, Test.p3.dictionary)
-        self.assertEquals(len(b.words), 7)
+        self.assertEquals(len(self.board), 9)
 
     def test_tlw_00(self):
         """Test all three letter words from 0,0."""
-        expect = set(['cat', 'cad', 'cot', 'cod', 'cog', 'coo'])
-        b = Board(3, Test.p3.grid, Test.p3.dictionary)
+        expect = set(['hod', 'gog', 'cod', 'tad', 'tog', 'oho', 'ago', 'dog',
+                      'cat', 'hot', 'dot', 'cot', 'goa', 'oat', 'ado', 'tod',
+                      'god', 'got', 'tag', 'goo', 'coo', 'doc', 'cog', 'tao',
+                      'cad', 'gad', 'hog', 'too'])
+        observed = {y[0] for y in self.all_words[3]}
 
-        observed = set(b.words.values())
         self.assertSetEqual(expect, observed)
 
 
